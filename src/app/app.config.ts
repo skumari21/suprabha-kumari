cad1fbfde34033171f6d1e14be85cdcad9db3c99
S Kumari <suprabhasinha2112@gmail.com>
2025-05-12 14:16:53 +0530

    update app.config routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
-import { CertificatesComponent } from './components/certificates/certificates.component';
 import { AppComponent } from './app.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: '',
-    component: AppComponent
-  },
-  {
-    path: 'home',
     component: AppComponent
   },
   {
