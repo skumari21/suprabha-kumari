68d4aa63ca283369f8a8e5facac98cdef30da027
S Kumari <suprabhasinha2112@gmail.com>
2025-05-13 13:55:21 +0530

    personal project fixes

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrls: ['./project-card.component.scss']
+  styleUrls: ['./project-card.component.scss'],
+  imports: [CommonModule]
 })
 export class ProjectCardComponent {
   @Input() title: string = '';
