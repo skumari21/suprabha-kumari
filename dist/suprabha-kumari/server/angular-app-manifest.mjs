43ed7653a5e6955d35b73c14bafc256e7c8eb307
S Kumari <suprabhasinha2112@gmail.com>
2026-01-19 22:30:21 +0530

    changes

--- dist/suprabha-kumari/server/angular-app-manifest.mjs
+++ dist/suprabha-kumari/server/angular-app-manifest.mjs
@@ -16,9 +16,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2503, hash: '326567b3626d945f10285993f97ff66b143f6d0190847eac0d89c5beccca1ea1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1734, hash: '17b598795be8fff1adc78fcb4298288916d1768edfe1cb35ff6a97cb9b1decb7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 51922, hash: '6dfb71fbeae1356da6ce4cb4bc34c1e0d3aca43dd149d0b0074c330784c397f3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2503, hash: '541bbd028f72c006b7ed18b4296bac3485ff847852b7b54ffeebffa8ce1bac4c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1734, hash: '4cf128e51b120028f0aec05defea92170f7d93720210e3febeb543876c5fb192', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 51942, hash: 'f0ce22dd7dccc3a67fef9fc9b68325c2a916631a18012b5e9a97cbb4c233b018', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5O7ZU3VD.css': {size: 75716, hash: 'OMaQTp/5cqA', text: () => import('./assets-chunks/styles-5O7ZU3VD_css.mjs').then(m => m.default)}
   },
 };
